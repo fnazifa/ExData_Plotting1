9444510196d18071d6789cd2a3125c8a1d97f094
Farah Nazifa <fnazifa@gmail.com>
2019-06-08 00:05:18 +0600

    Second commit - plot1.R constructs plot1.png plot

--- plot1.R
+++ plot1.R
@@ -1,18 +1,19 @@
-#Reading, naming and subsetting power consumption data
+# Reading the data
 power <- read.table("household_power_consumption.txt", sep=";")
 colnames(power) = as.character(unlist(power[1,]))
 power = power[-1, ]
 head(power)
 
+# Subsetting the data
 subpower <- subset(power, power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-#calling the basic plot function
+# calling the basic plot function
 hist(as.numeric(as.character(subpower$Global_active_power)), col="red", 
      main="Global Active Power", xlab="Global Active Power(kilowatts)")
 
 # annotating graph
 title(main="Global Active Power")
 
-dev.copy(png,"plot1.png", width=480, height=480)
+# Construct the plot and save it to a PNG file
+dev.copy(png, "plot1.png", width=480, height=480)
 dev.off()
-
